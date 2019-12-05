0fa5bec6bcd996325bd27b149d7b11271d726bcd
Niklas Hjelm <carrey_niklas@hotmail.com>
2019-12-05 11:32:41 +0100

    Fix me! I'm breaking SOLID!

---
+++ SOLID/src/sample/Animal.java
@@ -0,0 +1,21 @@
+package sample;
+
+public abstract class Animal {
+
+    public String _color;
+
+    public Animal(String color)
+    {
+        _color = color;
+    }
+
+    abstract void speak();
+
+    abstract void sleep();
+
+    abstract void eat();
+
+    public String getColor() {
+        return _color;
+    }
+}
