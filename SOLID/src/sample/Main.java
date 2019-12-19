d1b67f8d95a118c0cdaa097a0ba6f7f20226c481
NiklasHj <niklas.hjelm@ga.ntig.se>
2019-12-19 09:03:54 +0100

    Interfaced it!

--- SOLID/src/sample/Main.java
+++ SOLID/src/sample/Main.java
@@ -1,55 +1,50 @@
 package sample;
 
 
+import animals.*;
+
 public class Main   {
 
     public static void main(String[] args) {
 
-        Animal[] animals = {new Dog("Black"),
-                            new Hedgehog("Pink"),
-                            new Bird("Yellow"),
-                            new Snake("Black and Yellow")};
+        Animal[] animals = {new Dog("Black", "Its a dog!"),
+                            new Hedgehog("Blue", "Sonic!!"),
+                            new Duck("Yellow", "Its a Duck!"),
+                            new Owl("Brown", "Papa near!!"),
+                            new Snake("Black and Yellow", "Holy heck its Snek!")};
 
         System.out.println("###############");
         System.out.println();
 
+        activateAnimal(animals);
+    }
+
+    private static void activateAnimal(Animal[] animals) {
         for (Animal animal : animals) {
-            activateAnimal(animal);
+            animal.eat();
+            animal.sleep();
+            animal.speak();
+            animal.setColor("Grey");
+            animal.printDescritption();
+            System.out.println();
+
+            move(animal);
+
+            System.out.println("###############");
+            System.out.println();
         }
     }
 
-    private static void activateAnimal(Animal animal) {
-        System.out.println();
-
-        animal.eat();
-        animal.sleep();
-        animal.speak();
-        animal.walk();
-
-        printAnimalName(animal);
-
-        System.out.println("###############");
-        System.out.println();
-    }
-
-    private static void printAnimalName(Animal animal) {
-        switch (animal.getClass().getSimpleName())
-        {
-            case "Dog":
-                System.out.println("Its a dog!");
-                break;
-            case "Bird":
-                System.out.println("Its some kind of Bird!");
-                break;
-            case "Hedgehog":
-                System.out.println("Sonic?");
-                break;
-            case "Snake":
-                System.out.println("Oh heck its a Snaek!");
-                break;
-            default:
-                System.out.println("ANIMAL!");
-                break;
-        }
+    private static void move(Animal animal) {
+        animal.dash();
+        animal.fly();
+        animal.jump();
+        animal.roll();
+        animal.sit();
+        animal.run();
+        animal.skip();
+        animal.slither();
+        animal.trot();
+        animal.swim();
     }
 }
